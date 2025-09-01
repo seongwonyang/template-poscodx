670f40a5d9d188c4d672cec2700fa5e0f6a2fa86
seongwonyang <yang1762@uengine.org>
2025-09-01 13:22:28 +0900

    header info update

--- msaez0-proxy/src/main/java/com/poscodx/sample/msaez0/proxy/client/projectClient.java
+++ msaez0-proxy/src/main/java/com/poscodx/sample/msaez0/proxy/client/projectClient.java
@@ -6,6 +6,17 @@ fileName: {{nameCamelCase}}Client.java
 import com.poscodx.reuse.common.audit.web.JsonWebTokenReader;
 import com.poscodx.reuse.common.audit.web.AuthorizedUserInfo;
 
+/**
+ * Auto generated class
+ * 
+ * 자동생성 프로그램 버전 : 1.0.0
+ * 생성일시 :  {{currentTimestamp}}
+ * @FileName : 클래스에 대한 한글 명칭
+ * Change history
+ * @수정날짜;SCR_NO;수정자;수정내용
+ * @{{currentTimestamp}};00000;홍길동;최초생성
+ * 
+ */
 @RestController
 @RequestMapping("/api/v1/projects")
 public class projectClient {
--- msaez0-store/src/main/java/com/poscodx/sample/msaez0/store/domain/logic/ProjectLogic.java
+++ msaez0-store/src/main/java/com/poscodx/sample/msaez0/store/domain/logic/ProjectLogic.java
@@ -19,11 +19,11 @@ import com.poscodx.sample.{{boundedContext.nameCamelCase}}.store.domain.entity.{
  * Auto generated class
  * 
  * 자동생성 프로그램 버전 : 1.0.0
- * 생성일시 :  2025-06-19 11:29:02.759
+ * 생성일시 :  {{currentTimestamp}}
  * @FileName : 클래스에 대한 한글 명칭
  * Change history
  * @수정날짜;SCR_NO;수정자;수정내용
- * @2025-06-19 11:29:02.759;00000;홍길동;최초생성
+ * @{{currentTimestamp}};00000;홍길동;최초생성
  * 
  */
 @Slf4j
